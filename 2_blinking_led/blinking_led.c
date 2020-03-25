a98d9f592710a2fd8f80b4bcda65635d022adf1f
limaomao <1114320079@qq.com>
2020-03-25 21:51:33 +0800

    blinking led

--- 2_blinking_led/blinking_led.c
+++ 2_blinking_led/blinking_led.c
@@ -1,10 +1,18 @@
 #include <8051.h>
-#include <delay.h>
 #define led P2_0
 void main(void)
 {
 	while(1)	
 	{
-		
+		int i = 0;
+		int j = 0;
+		led = 0;
+		for(i=0; i<100; ++i)
+			for(j=0; j<100; ++j)
+			{}
+		led = 1;
+		for(i=0; i<100; ++i)
+			for(j=0; j<100; ++j)
+			{}
 	}
 }
